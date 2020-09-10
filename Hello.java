dac6f8a39bd8f5e809f2ff418c973674ccd07c9e
choudhryq <71049854+choudhryq@users.noreply.github.com>
2020-09-10 15:18:29 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+	public static void main(String args[]){
+		System.out.println("Annie Choudhry");
+		System.out.println("Lab 1");
+		System.out.println("Learning basics of java");
+		System.out.println("CMSC 255 001");
+	}
+}
